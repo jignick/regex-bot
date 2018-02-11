eafeae19d3bdb5a0f0ae74b95376a2a00087eb07
oikpanagiotis <OPanagiotis@gmail.com>
2018-02-11 18:35:44 +0200

    added message handler

---
+++ bot-bot/Services/MessageHandler.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using Discord.Net;
+using Discord.WebSocket;
+
+namespace bot_bot
+{
+    public class MessageHandler
+    {
+        private DiscordSocketClient _client;
+
+        public MessageHandler(DiscordSocketClient client)
+        {
+            _client = client;
+            _client.MessageReceived += ClearMessageAsync;
+        }
+
+        private async Task ClearMessageAsync(SocketMessage socketMessage)
+        {
+            var message = socketMessage as SocketUserMessage;
+
+            if (message == null)
+            {
+                return;
+            }
+
+            if (message.Content.StartsWith("!")) 
+            {
+                await message.DeleteAsync();
+            }
+
+            if (message.Author.Username == "Rythm")
+            {
+                await _client.GetGuild(152690567720075264).GetTextChannel(404659774043717632).SendMessageAsync($"{message.Content}");            
+                await message.DeleteAsync();     
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}
